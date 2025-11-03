e336c813799209b0abcafe0fb978b92bf4441660
gkokozides <kokozides.g@outlook.com>
2025-11-03 21:30:06 +0200

    Add project files.

---
+++ PagingPackage/PagingParameters.cs
@@ -0,0 +1,26 @@
+﻿namespace PagingPackage
+{
+    /// <summary>
+    /// Represents a set of parameters used to control paging, filtering, and sorting of query results.
+    /// </summary>
+    /// <remarks>Use this class to specify the page number, page size, and optional filtering or sorting
+    /// criteria when retrieving paged data from a source. The maximum allowed page size is 50; if a larger value is
+    /// specified, it will be capped at this limit. This type is commonly used in APIs or data access layers to
+    /// standardize pagination and query options.</remarks>
+    public class PagingParameters
+    {
+        private const int MaxPageSize = 100;
+
+        public int PageNumber { get; set; } = 1;
+        //default page size
+        private int _pageSize = 10;
+        // if the user specifies a page size greater than my maxpagesize then i set it as the maxpagesize otherwise i set it as the value of the user
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+        public string SortBy { get; set; }
+        public bool SortDesc { get; set; } = false;
+    }
+}
---
+++ PagingPackage/PagingToolkit.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PagingPackage
+{
+    public class PagingToolkit<T> : List<T>
+    {
+        public PagingToolkit(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageSize = pageSize;
+            TotalCount = count;
+            AddRange(items);
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public static async Task<PagingToolkit<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize, CancellationToken ct)
+        {
+            var count = await source.CountAsync(ct);
+            var items = await source.Skip(pageNumber * pageSize).Take(pageSize).ToListAsync(ct);
+            return new PagingToolkit<T>(items, count, pageNumber, pageSize);
+        }
+
+        public static PagingToolkit<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip(pageNumber * pageSize).Take(pageSize).ToList();
+            return new PagingToolkit<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
